1e3c8cf5b4cc6bc0e856ce55c9c6d9d7954f7f4c
Ann-Sanchala-Kavindi <annsanchala@gmail.com>
2026-05-28 09:52:32 +0530

    First Commit

---
+++ Smart-Inventory-Prediction-System/src/main/java/com/Smart/Inventory/Prediction/System/SmartInventoryPredictionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Smart.Inventory.Prediction.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInventoryPredictionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInventoryPredictionSystemApplication.class, args);
+	}
+
+}
